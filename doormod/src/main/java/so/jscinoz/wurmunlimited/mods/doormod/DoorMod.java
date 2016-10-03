e886f152d57ef097c8f25a241ebfff8d2ca929a7
Jack Coulter <jscinoz@jscinoz.so>
2016-10-03 17:55:48 +1100

    Implement structure door mod using ByteBuddy

--- doormod/src/main/java/so/jscinoz/wurmunlimited/mods/doormod/DoorMod.java
+++ doormod/src/main/java/so/jscinoz/wurmunlimited/mods/doormod/DoorMod.java
@@ -1,21 +1,36 @@
 package so.jscinoz.wurmunlimited.mods.doormod;
 
 import org.gotti.wurmunlimited.modloader.classhooks.HookManager;
-import org.gotti.wurmunlimited.modloader.classhooks.HookException;
 import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
 import org.gotti.wurmunlimited.modloader.interfaces.PreInitable;
 
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.pool.TypePool;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.ClassFileLocator;
 
-// import so.jscinoz.wurmunlimited.mods.common.BaseMod;
+import javassist.Loader;
 
 import static java.util.logging.Level.INFO;
 
 public class DoorMod implements WurmServerMod, PreInitable {
+  private static final String STRUCTURE_CLASS =
+    "com.wurmonline.server.structures.Structure";
+
   @Override
   public void preInit() {
+    Loader loader = HookManager.getInstance().getLoader();
+    TypePool pool = TypePool.Default.ofClassPath();
+    TypeDescription structureType = pool.describe(STRUCTURE_CLASS).resolve();
+    ClassFileLocator locator = ClassFileLocator.ForClassLoader.ofClassPath();
+
     new ByteBuddy()
-      .rebase(com.wurmonline.server.structures.Structure.class);
-    throw new HookException("TODO");
+      .redefine(structureType, locator)
+      .method(ElementMatchers.named("needsDoor"))
+      .intercept(FixedValue.value(false))
+      .make()
+      .load(loader);
   }
 }
